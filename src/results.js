f2579c07bbd3de5741802e17aec7515db21706d6
Archit Kejriwal <architkejriwal@MacBook-Pro.local>
2018-09-18 15:44:51 +0530

    highcharts midway

--- src/results.js
+++ src/results.js
@@ -11,6 +11,9 @@ import VALUES, {
   usableRoof
 } from "./values";
 
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
 class Results extends Component {
   compoundedValue = (value, rate, years) => {
     return value * Math.pow(1 + rate / 100, years);
@@ -81,20 +84,81 @@ class Results extends Component {
 
     const annualSavingsArray = annualConsumptionSSArray.map((ac, index) => {
       const gr = govtRateArray[index];
-      return ac * gr * (1 - ppaFactor);
+      return Math.round((ac * gr * (1 - ppaFactor)) / 10) * 10;
     });
 
     const cumulativeSavingsArray = annualSavingsArray.reduce(
       (sumArray, item) => {
         if (sumArray.length === 0) {
-          sumArray.push(item);
+          sumArray.push(Math.round(item / 100) * 100);
           return sumArray;
         }
-        sumArray.push(item + sumArray[sumArray.length - 1]);
+        sumArray.push(
+          Math.round((item + sumArray[sumArray.length - 1]) / 100) * 100
+        );
         return sumArray;
       },
       []
     );
+
+    const options = {
+      chart: {
+        type: "spline"
+      },
+      title: {
+        text: "Here are your Savings!"
+      },
+      subtitle: {
+        text:
+          "Swipe over the map to see your annual and cumulative savings by year"
+      },
+      series: [
+        {
+          name: "Annual Savings",
+          // marker: "diamond",
+          data: annualSavingsArray
+        },
+        {
+          name: "Total Savings till date",
+          // marker: "circle",
+          data: cumulativeSavingsArray
+        }
+      ],
+      xAxis: {
+        categories: [
+          "Year 1",
+          "Year 2",
+          "Year 3",
+          "Year 4",
+          "Year 5",
+          "Year 6",
+          "Year 7",
+          "Year 8",
+          "Year 9",
+          "Year 10",
+          "Year 11",
+          "Year 12",
+          "Year 13",
+          "Year 14",
+          "Year 15",
+          "Year 16",
+          "Year 17",
+          "Year 18",
+          "Year 19",
+          "Year 20",
+          "Year 21",
+          "Year 22",
+          "Year 23",
+          "Year 24",
+          "Year 25"
+        ]
+      },
+      tooltip: {
+        shared: true,
+        followPointer: true,
+        crosshairs: true
+      }
+    };
 
     return (
       <div className="result">
@@ -103,6 +167,10 @@ class Results extends Component {
         <div>{roof}</div>
         <div>{roofUnit}</div>
         <div>{place}</div>
+
+        <div>
+          <HighchartsReact highcharts={Highcharts} options={options} />
+        </div>
 
         <div style={{ whiteSpace: "pre" }}>{annualSavingsArray.join("\n")}</div>
         <div style={{ whiteSpace: "pre" }}>
